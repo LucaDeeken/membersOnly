aae51e40ecce49b7e1eec8b47a0cb46faee016b0
Luca Deeken <luca.deeken@fuchs-edv.de>
2026-04-16 15:21:11 +0200

    feat: init express.js and prismaSetup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("grützki!"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`listening on port ${PORT}!`);
+});
